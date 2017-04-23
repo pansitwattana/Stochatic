099e910a51d420b7d3ad594bf0fdb5b3d4ae9a4e
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2017-04-23 13:34:40 +0700

    add ce, cd

--- graph.m
+++ graph.m
@@ -1,5 +1,5 @@
 E = 0.1:0.1:15.0;
-plot(E,log(pc),E,log(pd),E,log(ppe))
+plot(E,log(cplot),E,log(eplot))
 xlabel('The signal energy E')
-ylabel('The probability of error')
-legend('C','D','E')
+ylabel('The Probability of error')
+legend('P(m0) = P(m1) = 0.5 , var1=var2=var3=1','P(m0) = P(m1) , var1=1, var2=2, var3=4')
