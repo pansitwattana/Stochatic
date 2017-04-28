bd1901030c0897aa5524886420a10312ab15bc61
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2017-04-29 00:09:49 +0700

    add hcd and hce graph

--- graph.m
+++ graph.m
@@ -1,6 +1,6 @@
 E = 0.1:0.1:15.0;
-plot(E,log10(fc),E,log10(fd))
+plot(E,log10(hc),E,log10(he))
 grid on
 xlabel('The signal energy E')
 ylabel('The Probability of error')
-legend('P(m0) = P(m1), var1=var2=var3=1','P(m0) = ¼ P(m1) = ¾  , var1=var2=var3=1')
+legend('P(m0) = P(m1), var1=var2=var3=1','P(m0) = P(m1), var1=1 var2=2 var3=4')
