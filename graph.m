871050a94d6790df0b76d85af1c5a638ed35b859
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2017-04-29 23:09:19 +0700

    fix picture

--- graph.m
+++ graph.m
@@ -1,6 +1,6 @@
 E = 0.1:0.1:15.0;
-plot(E,log10(hc),E,log10(he))
+plot(E,log10(hc),E,log10(hd))
 grid on
 xlabel('The signal energy E')
 ylabel('The Probability of error')
-legend('P(m0) = P(m1), var1=var2=var3=1','P(m0) = P(m1), var1=1 var2=2 var3=4')
+legend('P(m0) = P(m1), var1=var2=var3=1','P(m0) = 0.25, P(m1) = 0.75, var1=var2=var3')
